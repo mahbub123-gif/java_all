441b2ad95b95d8c18c9fe2caea06c50c54a2ef1b
Mahbub Hasan <63605025+mahbub123-gif@users.noreply.github.com>
2021-10-02 22:45:38 +0600

    Add files via upload

---
+++ lab 6/src/lab/lab4prblmc.java
@@ -0,0 +1,18 @@
+package lab 6;
+
+public class lab4prblmc {
+    public static void main(String[] args) {
+        Icecream obj1=new Icecream();
+        obj1.icecreamType="vanilla";
+        obj1.icecreamCompany="Igloo";
+        obj1.icecreamPrice= 10;
+        Icecream obj2=new Icecream();
+        obj2.icecreamType="Chocolate";
+        obj2.icecreamCompany="Igloo";
+        obj2.icecreamPrice=20;
+        obj1.toString();
+        obj2.toString();
+        obj1.display();
+        obj2.display();
+    }   
+}
